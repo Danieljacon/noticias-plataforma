5259b2a3380faa0d93d297902754a53c284a33a3
Danieljacon <danielcamillo2020@gmail.com>
2022-06-23 20:45:31 -0300

    Componente de Social, para Social Media, foi adicionado.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,19 @@
 import { GithubLogo, LinkedinLogo } from "phosphor-react";
 import logo_danielJacon from "../assets/logo_daniel-jacon.svg";
+import Social from "./Social";
 
 const Header = () => {
   return (
     <header className="flex pt-5 justify-between pb-4">
       <div className="flex gap-4">
-        <a href="" className="flex items-center  text-gray-900 font-bold">
-          <div className="w-11 h-11 bg-gray-900 rounded-[4px] flex justify-center items-center">
-            <LinkedinLogo size={28} color="#fcfcfc" weight="bold" />
-          </div>
-          Linkedin
-        </a>
-
-        <a href="" className="flex items-center  text-gray-900 font-bold">
-          <div className="w-11 h-11 bg-gray-900 rounded-[4px] flex justify-center items-center">
-            <GithubLogo size={28} color="#fcfcfc" weight="bold" />
-          </div>
-          Github
-        </a>
+        <Social
+          icon={<LinkedinLogo size={28} color="#fcfcfc" weight="bold" />}
+          title="Linkedin"
+        />
+        <Social
+          icon={<GithubLogo size={28} color="#fcfcfc" weight="bold" />}
+          title="Github"
+        />
       </div>
 
       <img src={logo_danielJacon} alt="Logo Daniel Jacon" />
---
+++ src/components/Social.tsx
@@ -0,0 +1,18 @@
+interface Props {
+    icon: React.ReactNode;
+    title: string;
+    url?: string;
+}
+
+const Social = (props: Props) => {
+  return (
+    <a href={props.url} className="flex items-center  text-gray-900 font-bold">
+      <div className="w-11 h-11 bg-gray-900 rounded-[4px] flex justify-center items-center">
+        {props.icon}
+      </div>
+      {props.title}
+    </a>
+  );
+};
+
+export default Social;
