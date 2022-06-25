b0f1c21a80b79f059a2a5f784cfd14ff33197041
Danieljacon <danielcamillo2020@gmail.com>
2022-06-25 12:21:54 -0300

    Rotas adicionadas com o React Router Dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import Home from "./pages/Home"
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./lib/apollo";
+import { BrowserRouter as Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Router from "./Router";
 
 const App = () => {
   return (
-    <main className="max-w-[1390px] m-auto">
-      <Home />
-    </main>
-  )
-}
+    <ApolloProvider client={client}>
+      <Routes>
+        <Router />
+      </Routes>
+    </ApolloProvider>
+  );
+};
 
-export default App
+export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,18 +3,21 @@ import News from "../components/News";
 import NewsContent from "../components/NewsContent";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useParams } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen bg-gray-50">
-      <div className="flex-1 flex flex-col px-[3.75rem] pb-[3.75rem]">
-        <Header />
-        <News />
+    <main className="max-w-[1390px] m-auto">
+      <div className="flex min-h-screen bg-gray-50">
+        <div className="flex-1 flex flex-col px-[3.75rem] pb-[3.75rem]">
+          <Header />
+          <News />
+        </div>
+        <main className="flex-1 max-w-[622px] bg-gray-900">
+          <NewsContent />
+        </main>
       </div>
-      <main className="flex-1 max-w-[622px] bg-gray-900">
-        <NewsContent />
-      </main>
-    </div>
+    </main>
   );
 };
 
